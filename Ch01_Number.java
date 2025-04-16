5597b06d0cdbd146274d8282e7c0274123fdb11c
5647kr <5647kr@gmail.com>
2025-04-16 20:35:21 +0900

    Ch01_Number

---
+++ Ch01_Number.java
@@ -0,0 +1,9 @@
+public class Ch01_Number {
+  // 데이터 타입: 숫자
+  public static void main(String[] args) {
+    System.out.println("1 + 2 = " + (1 + 2));
+    System.out.println("1.2 + 1.3 = " + (1.2 + 1.3));
+    System.out.println("2 * 3 = " + (2 * 3));
+    System.out.println("6 / 2 = " + (6 / 2));
+  }
+}
