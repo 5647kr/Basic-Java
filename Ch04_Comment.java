885eba8725b72942a233e816d8e833b2e7bb2575
5647kr <5647kr@gmail.com>
2025-04-17 15:27:09 +0900

    Ch04_Comment

---
+++ Ch04_Comment.java
@@ -0,0 +1,19 @@
+public class Ch04_Comment {
+  public static void main(String[] args) {
+    // 주석
+    System.out.println("==========한 줄 주석==========");
+    System.out.println("// 이용해 한 줄 주석을 사용할 수 있다.");
+    // 한 줄 주석입니다.
+
+    System.out.println("==========여러 줄 주석==========");
+    System.out.println("/* */ 이용해 한 줄 주석을 사용할 수 있다.");
+    
+    /* 여러 줄 주석입니다.
+    * 여러 줄 주석입니다.
+    * 여러 줄 주석입니다.
+    */
+    
+    System.out.println("==========세미콜론==========");
+    // 문장의 끝을 의미한다. 자바에서는 문장의 끝에 세미콜론을 사용하지 않으면 컴파일 에러가 발생한다.
+  }
+}
