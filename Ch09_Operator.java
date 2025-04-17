037ed0992a1ed3fa3047325ac3076a8615ab2032
5647kr <5647kr@gmail.com>
2025-04-17 15:29:14 +0900

    Ch09_Operator

---
+++ Ch09_Operator.java
@@ -0,0 +1,20 @@
+public class Ch09_Operator {
+  public static void main(String[] args) {
+    // 산술 연산자(+, -, *, /, %)
+
+    int result1 = 1 + 2;
+    System.out.println("1 + 2 = " + result1);
+
+    int result2 = 2 - 1;
+    System.out.println("2 - 1 = " + result2);
+
+    int result3 = 2 * 2;
+    System.out.println("2 * 2 = " + result3);
+
+    int result4 = 4 / 2;
+    System.out.println("4 / 2 = " + result4);
+
+    int result5 = 5 % 3;
+    System.out.println("5 % 3 = " + result5);
+  }
+}
