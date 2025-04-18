f90895452e1163b10189b37d34e21ff5328acbcb
5647kr <5647kr@gmail.com>
2025-04-18 21:02:54 +0900

    Ch14_Condition

---
+++ Ch14_Condition.java
@@ -0,0 +1,18 @@
+public class Ch14_Condition {
+  public static void main(String[] args) {
+    // 논리 연산자
+
+    // and(&&) 연산자
+    if(true && true) {
+      System.out.println(1);
+    }
+
+    if(true && false) {
+      System.out.println(2);
+    }
+
+    if(false && false) {
+      System.out.println(3);
+    }
+  }
+}
