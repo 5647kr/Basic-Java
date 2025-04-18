df00a380bafe9b06e32130baf53f77d6d0b77ef9
5647kr <5647kr@gmail.com>
2025-04-18 21:02:45 +0900

    Ch13_Switch

---
+++ Ch13_Switch.java
@@ -0,0 +1,21 @@
+public class Ch13_Switch {
+  public static void main(String[] args) {
+    // switch 문
+    switch(5) {
+      case 1:
+        System.out.println("one");
+        break;
+      case 2:
+        System.out.println("two");
+        break;
+      case 3:
+        System.out.println("three");
+        break;
+      case 4:
+        System.out.println("four");
+        break;
+      default:
+        System.out.println("default");
+    }
+  }
+}
