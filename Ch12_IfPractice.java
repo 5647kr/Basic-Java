f4ec5b6da31eed40cec514aff17e06fb090f3818
5647kr <5647kr@gmail.com>
2025-04-18 21:02:24 +0900

    Ch13_IfPractice

---
+++ Ch12_IfPractice.java
@@ -0,0 +1,10 @@
+public class Ch12_IfPractice {
+  public static void main(String[] args) {
+    
+    if(4 < 3 && 2 > 1) {
+      System.out.println("true");
+    } else {
+      System.out.println("false");
+    }
+  }
+}
