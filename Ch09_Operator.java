e3044be4f233287dc805549ff59126aa1d32a774
5647kr <5647kr@gmail.com>
2025-04-18 21:01:57 +0900

    Ch09_Operator

--- Ch09_Operator.java
+++ Ch09_Operator.java
@@ -16,5 +16,27 @@ public class Ch09_Operator {
 
     int result5 = 5 % 3;
     System.out.println("5 % 3 = " + result5);
+
+    // 연산 형 변환
+    int result6 = 10 / 3;
+    System.out.println("10 / 3 = " + result6);
+
+    float result7 = 10.0F / 3.0F;
+    System.out.println("10.0F / 3.0F = " + result7);
+
+    System.out.println("10 / 3.0F = " + (10 / 3.0F));
+
+
+    System.out.println("========== 단항 연산자 ==========");
+    
+    int num1 = 1;
+    num1++; // num1 = num1 + 1;
+    System.out.println("num1: " + num1);
+
+
+    System.out.println("========== 연산자 우선순위 ==========");
+
+    int num2 = 4 - 3 * 6;
+    System.out.println("num2: " + num2);
   }
 }
