c5093fe394da6ff7f4035a4f772f4b17661fed2b
5647kr <5647kr@gmail.com>
2025-04-21 21:48:07 +0900

    Ch14_Condition

--- Ch14_Condition.java
+++ Ch14_Condition.java
@@ -4,15 +4,38 @@ public class Ch14_Condition {
 
     // and(&&) 연산자
     if(true && true) {
-      System.out.println(1);
+      System.out.println("and 연산자1 출력");
     }
 
     if(true && false) {
-      System.out.println(2);
+      System.out.println("and 연산자2 출력");
     }
 
     if(false && false) {
-      System.out.println(3);
+      System.out.println("and 연산자3 출력");
     }
+
+    // or(||) 연산자
+    if(true || true) {
+      System.out.println("or 연산자1 출력");
+    }
+    
+    if(true || false) {
+      System.out.println("or 연산자2 출력");
+    }
+    
+    if(false || false) {
+      System.out.println("or 연산자3 출력");
+    }
+
+    // not(!) 연산자
+    if(!true) {
+      System.out.println("not 연산자1 출력");
+    }
+    
+    if(!false) {
+      System.out.println("not 연산자2 출력");
+    }
+  
   }
 }
