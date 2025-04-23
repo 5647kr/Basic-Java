0308dd904efbd1ebfe745fd7f0bfbde6d6263bf1
5647kr <5647kr@gmail.com>
2025-04-23 22:18:03 +0900

    Ch20_Input

---
+++ Ch20_Input.java
@@ -0,0 +1,27 @@
+public class Ch20_Input {
+  // 매개변수와 인자
+
+  // 매개변수(parameter)
+  public static void numbering1(int limit) {
+    int i = 0;
+    while (i < limit) {
+      System.out.println("매개변수 1개: " + i);
+      i++;
+    }
+  }
+
+  public static void numbering2(int init, int limit) {
+    int i = init;
+    while (i < limit) {
+      System.out.println("매개변수2개이상: "+ i);
+      i++;
+    }
+  }
+
+  public static void main(String[] args) {
+    // 인자(argument)
+    numbering1(5);
+    numbering2(3, 5);
+  }
+
+}
