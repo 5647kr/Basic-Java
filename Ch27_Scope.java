76b75567ef17b6576430a2149179821cbf062191
5647kr <5647kr@gmail.com>
2025-04-29 15:33:34 +0900

    Ch27_Scope

---
+++ Ch27_Scope.java
@@ -0,0 +1,13 @@
+public class Ch27_Scope {
+
+  static void num() {
+    int i = 0;
+  }
+
+  public static void main(String[] args) {
+    for(int i = 0; i < 5; i++) {
+      num();
+      System.out.println("num: " + i);
+    }
+  }
+}
