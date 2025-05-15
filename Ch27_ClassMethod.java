7525b95fddb29dabbee5379e2aedae02eeea24fd
5647kr <5647kr@gmail.com>
2025-05-15 12:17:27 +0900

    Ch27_ClassMethod

--- Ch27_ClassMethod.java
+++ Ch27_ClassMethod.java
@@ -4,6 +4,7 @@ class Calculator {
   // 인스턴스 변수 => non-static field
   // 클래스 변수 => static field
   
+  // sum이란 메소드에 static 키워드를 붙이면 클래스 메소드가 된다. 클래스에 직접 접근해서 사용할 수 있다.
   public static void sum (int left, int right) {
     System.out.println("sum: " + (left + right));
   }
@@ -13,7 +14,7 @@ class Calculator {
   }
 }
 
-public class Ch26_ClassMethod {
+public class Ch27_ClassMethod {
   public static void main(String[] args) {
     Calculator.sum(10, 20);
     Calculator.avg(10, 20);
