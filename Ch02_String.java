9bbcbd3959ee1ff9d36bc01be6051301430e8ba5
5647kr <5647kr@gmail.com>
2025-05-18 12:00:00 +0900

    Ch02_String

--- Ch02_String.java
+++ Ch02_String.java
@@ -12,19 +12,19 @@ public class Ch02_String {
     // System.out.println('apple'); Error
 
     // 문자열을 결합할 땐 + 연산자를 사용한다.
-    System.out.println("==========문자열 결합==========");
+    System.out.println("========== 문자열 결합 ==========");
     System.out.println("코딩" + "입니다");
 
     // "1" + "1" 은 숫자같지만 문자로 인식하여 결합한다.
-    System.out.println("==========숫자 결합==========");
+    System.out.println("========== 숫자 결합 ==========");
     System.out.println("1" + "1");
 
     // 문자열 안에 쌍따옴표를 표현하려면
-    System.out.println("==========문자열내 쌍따옴표 표현==========");
+    System.out.println("========== 문자열내 쌍따옴표 표현 ==========");
     System.out.println("안녕하세요 \"코딩입니다.\"");
 
     // 문자열을 여러줄로 표현하려면
-    System.out.println("==========문자열 여러줄 표현==========");
+    System.out.println("========== 문자열 여러줄 표현 ==========");
     System.out.println("안녕하세요 \n코딩입니다.");
   }
 }
