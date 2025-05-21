57ed232a4cbafe9d5e9b09b16990010b5bc3ae57
5647kr <5647kr@gmail.com>
2025-05-22 08:57:18 +0900

    Ch10_Boolean

--- Ch10_Boolean.java
+++ Ch10_Boolean.java
@@ -5,7 +5,7 @@ public class Ch10_Boolean {
     // 참과 거짓을 의미하는 데이터이다.
 
     
-    System.out.println("==========비교 연산자==========");
+    System.out.println("========== 비교 연산자 ==========");
     // 비교 연산자
     System.out.println("1 == 2: " + (1 == 2));
     System.out.println("1 == 1: " + (1 == 1));
@@ -13,7 +13,7 @@ public class Ch10_Boolean {
     System.out.println("one == one: " + ("one" == "one"));
     
 
-    System.out.println("==========Not 연산자==========");
+    System.out.println("========== Not 연산자 ==========");
     // Not 연산자
     System.out.println("1 != 2: " + (1 != 2));
     System.out.println("1 != 1: " + (1 != 1));
@@ -21,7 +21,7 @@ public class Ch10_Boolean {
     System.out.println("one != one: " + ("one" != "one"));
     
 
-    System.out.println("==========부등호 연산자==========");
+    System.out.println("========== 부등호 연산자 ==========");
     // 부등호 연산자
     System.out.println("1 > 2: " + (1 > 2));
     System.out.println("1 < 2: " + (1 < 2));
