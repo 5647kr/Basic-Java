92f8b7674eb48f41993926420d017fa255fb4520
5647kr <5647kr@gmail.com>
2025-05-22 08:57:52 +0900

    Ch13_Switch

--- Ch13_Switch.java
+++ Ch13_Switch.java
@@ -1,6 +1,8 @@
 public class Ch13_Switch {
   public static void main(String[] args) {
     // switch 문
+
+    System.out.println("========== 조건문(switch문) ==========");
     switch(5) {
       case 1:
         System.out.println("one");
