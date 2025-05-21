f055afe2b11c114ec15e9067f286b63dbcde9fc3
5647kr <5647kr@gmail.com>
2025-05-21 09:56:04 +0900

    Ch05_DataType

--- Ch05_DataType.java
+++ Ch05_DataType.java
@@ -13,7 +13,7 @@ public class Ch05_DataType {
      */
 
     // 정수형 데이터 타입
-    System.out.println("==========정수형 데이터 타입==========");
+    System.out.println("========== 정수형 데이터 타입 ==========");
     // 1. byte(1byte): -128 ~ 127
     // 2. short(2byte): -32,768 ~ 32,767
     // 3. int(4byte): -2,147,483,648 ~ 2,147,483,647
@@ -26,13 +26,16 @@ public class Ch05_DataType {
     // 결과적으로 정수를 표현할 땐 int를 사용을 권장한다.
     
     // 실수형 데이터 타입
-    System.out.println("==========실수형 데이터 타입==========");
+    System.out.println("========== 실수형 데이터 타입 ==========");
     // 1. float(4byte)
     // 2. double(4byte)
     // 결과적으로 실수를 표현할 땐 double 사용을 권장한다.
     
     // 문자 데이터 타입
-    System.out.println("==========문자 데이터 타입==========");
+    System.out.println("========== 문자 데이터 타입 ==========");
     // 결과적으로 문자를 표현할 땐 char 사용을 권장한다.
+
+    char ch1 = 'a';
+    System.out.println(ch1);
   }
 }
