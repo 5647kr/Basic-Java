6d3befdb6a55b888159ecc717a34f5fff1aa59e0
5647kr <5647kr@gmail.com>
2025-05-22 08:57:45 +0900

    Ch12_IfPractice

--- Ch12_IfPractice.java
+++ Ch12_IfPractice.java
@@ -1,6 +1,7 @@
 public class Ch12_IfPractice {
   public static void main(String[] args) {
-    
+
+    System.out.println("========== 조건문(if-else문) ==========");
     if(4 < 3 && 2 > 1) {
       System.out.println("true");
     } else {
