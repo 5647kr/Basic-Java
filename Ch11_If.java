44e8ee86c20e041db3956e3145e2288f74d1cd82
5647kr <5647kr@gmail.com>
2025-05-22 08:57:37 +0900

    Ch11_If

--- Ch11_If.java
+++ Ch11_If.java
@@ -8,11 +8,13 @@ public class Ch11_If {
      *  조건이 거짓일 떄, 실행할 코드
      * }
      */
+
+    System.out.println("========== 조건문(if문) ==========");
     if(true) {
       System.out.println("result: true");
     }
 
-
+    System.out.println("========== 조건문(if-else문) ==========");
     if(false) {
       System.out.println("result: true");
     } else {
