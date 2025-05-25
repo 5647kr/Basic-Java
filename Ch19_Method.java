531a40f1eaad9a0de67940d6e6dceb2fb3ff8f46
5647kr <5647kr@gmail.com>
2025-05-26 08:51:07 +0900

    Ch19_Method

--- Ch19_Method.java
+++ Ch19_Method.java
@@ -1,6 +1,7 @@
 public class Ch19_Method {
-    // 메소드의 정의
-  // js에서 함수와 비슷한 느낌
+  // js에서 함수
+
+  // 메소드의 정의
   public static void numbering() {
     int i = 0;
     while (i < 10) {
@@ -10,6 +11,8 @@ public class Ch19_Method {
   }
 
   public static void main(String[] args) {
+//    main이라고 하는것이 method이다.
+
     // 메소드의 호출
     numbering();
   }
