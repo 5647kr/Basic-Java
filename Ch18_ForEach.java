bc2fe2cede43685c6e0c0b6fe97e7dc0141a783c
5647kr <5647kr@gmail.com>
2025-05-26 08:51:01 +0900

    Ch18_ForEach

--- Ch18_ForEach.java
+++ Ch18_ForEach.java
@@ -1,14 +1,15 @@
 public class Ch18_ForEach {
   public static void main(String[] args) {
-    System.out.println("==========문자열 배열==========");
+    System.out.println("========== 문자열 배열 ==========");
     // 문자열 배열
     String[] arr1 = {"str1", "str2", "str3", "str4"};
+//    arr1을 순회하면서 안에 있는 데이터의 값을 e라는 변수에 저장한다.
     for(String e : arr1) {
       System.out.println("str: " + e);
     }
     
     
-    System.out.println("==========숫자형 배열==========");
+    System.out.println("========== 숫자형 배열 ==========");
     // 숫자형 배열
     int[] arr2 = {1, 2, 3, 4};
     for(int e : arr2) {
