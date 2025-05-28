ca78f348e6de5fe2329f20bf2dd0b789187c0afd
5647kr <5647kr@gmail.com>
2025-05-28 09:10:06 +0900

    Ch28_Scope

--- Ch28_Scope.java
+++ Ch28_Scope.java
@@ -1,13 +1,27 @@
 public class Ch28_Scope {
 
-  static void num() {
+//  지역 변수
+  static void num1() {
     int i = 0;
+  }
+
+//  전역 변수
+  static int j;
+
+  static void num2() {
+    j = 0;
   }
 
   public static void main(String[] args) {
     for(int i = 0; i < 5; i++) {
-      num();
-      System.out.println("num: " + i);
+      num1();
+      System.out.println("num1: " + i);
+    }
+
+//    무제한 실행
+    for(int j = 0; j < 5; j++) {
+      num2();
+      System.out.println("num2: " + j);
     }
   }
 }
