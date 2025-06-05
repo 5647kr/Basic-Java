c656d739f47930a1442888156d91a085bdc98855
5647kr <5647kr@gmail.com>
2025-06-05 09:12:11 +0900

    Ch45_Interface1

---
+++ Ch45_Interface1.java
@@ -0,0 +1,19 @@
+// Interface
+// 어떤 객체(클래스)가 있고 그 객체가 특정한 인터페이스를 사용한다면 그 객체는 반드시 그 인터페이스에 포함되어있는 메소드를 구현하도록 강제하는 것이 Interface이다.
+
+interface I45 {
+	public void a();
+}
+
+// class A45는 interface I45를 구현한다.
+class A45 implements I45 {
+	public void a() {
+//		구현
+	}
+}
+
+public class Ch45_Interface1 {
+	public static void main(String[] args) {
+
+	}
+}
