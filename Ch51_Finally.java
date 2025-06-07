fa0b703df40713c8afd4e20b3e26ab81c34a9908
5647kr <5647kr@gmail.com>
2025-06-07 19:40:48 +0900

    Ch51_Finally

---
+++ Ch51_Finally.java
@@ -0,0 +1,14 @@
+// try {
+//  예외의 발생이 예상되는 로직
+//} catch (예외 클래스 인스턴스) {
+//  예외가 발생했을 때 실행되는 로직
+//	} finally {
+//  예외 여부와 관계없이 실행되는 로직
+//	}
+
+
+public class Ch51_Finally {
+	public static void main(String[] args) {
+
+	}
+}
